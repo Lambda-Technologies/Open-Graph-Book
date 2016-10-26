9d7a10766d763d921bc8fefc3dc54cff22d925e3
Dustyn Tubbs <DustynTubbs@Gmail.com>
2016-10-26 10:14:54 -0500

    Oops, lets switch this over to express too

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,10 @@
-var koa = require('koa');
-var app = koa();
+var express = require('express');
+var app = express();
 
-console.log("Koa listening on localhost:3000")
-
-app.use(function *(){
-  this.body = 'Hello World';
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(3000);
-
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
